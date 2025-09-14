818cff4721683b02b4d381c171beeafdb3cede83
Liban Golo <liban.golo@student.moringaschool.com>
2025-09-14 22:18:14 +0300

    tasklister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+
+  const todoForm = document.getElementById("create-task-form")
+  const userInput = document.getElementById("new-task-description")
+  const taskList = document.getElementById("list");
+
+  const userTask = userInput.value;
+  todoForm.addEventListener("submit", function(event) {
+  event.preventDefault(); 
+
+  const userTask = userInput.value;
+
+  if (userTask === "") return; 
+
+  const li = document.createElement("li");
+  li.textContent = userTask;
+
+  taskList.appendChild(li);
+  userInput.value = ""; 
+
+ 
+});
+
+
+
+
+
+
 });
